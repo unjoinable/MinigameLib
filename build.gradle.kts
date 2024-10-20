d947759be771964d1c0fe3acf8eb64faa80e9a89
unjoinable <sneakyplayzminecraft@gmail.com>
2024-10-20 11:55:05 +0530

    Initial Commit

---
+++ build.gradle.kts
@@ -0,0 +1,12 @@
+plugins {
+    id("java")
+}
+
+group = "io.github.unjoinable"
+version = "1.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {}
---
+++ settings.gradle.kts
@@ -0,0 +1,2 @@
+rootProject.name = "MiniGameLib"
+include("common")
