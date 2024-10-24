0b63de5db7dbd9587495c25a75f9225b4d9afc67
unjoinable <sneakyplayzminecraft@gmail.com>
2024-10-24 14:32:22 +0530

    Trying to get project to publish to maven

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,6 +14,12 @@ subprojects {
     apply(plugin = "java")
     apply(plugin = "maven-publish")
 
+    java {
+        toolchain {
+            languageVersion = JavaLanguageVersion.of(23)
+        }
+    }
+
     dependencies {
         compileOnly("org.jetbrains:annotations:26.0.1")
         implementation("org.slf4j:slf4j-api:2.0.16")
