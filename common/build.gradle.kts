21e0ba79274f8de4fb85d8f97f79082f9178bd0c
unjoinable <sneakyplayzminecraft@gmail.com>
2024-10-25 20:58:09 +0530

    Simplified / Cleaned Build files

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -1,6 +1,3 @@
-group = "io.github.unjoinable"
-version = "1.0"
-
 dependencies {
     implementation("net.kyori:adventure-api:4.17.0")
 }
--- minestom/build.gradle.kts
+++ minestom/build.gradle.kts
@@ -2,9 +2,6 @@ plugins {
     id("com.github.johnrengelman.shadow") version "8.1.1"
 }
 
-group = "io.github.unjoinable"
-version = "1.0"
-
 dependencies {
     implementation(project(":common"))
     compileOnly("net.minestom:minestom-snapshots:b0bad7e180")
