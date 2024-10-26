1bc8bdc7d1ff9962ff320f349652976ea49b358d
unjoinable <sneakyplayzminecraft@gmail.com>
2024-10-26 13:23:07 +0530

    Changed base name

--- minestom/build.gradle.kts
+++ minestom/build.gradle.kts
@@ -2,3 +2,9 @@ dependencies {
     implementation(project(":"))
     compileOnly("net.minestom:minestom-snapshots:b0bad7e180")
 }
+
+tasks {
+    shadowJar {
+        archiveClassifier.set("")
+    }
+}
